96865d6336527895d0fff0b0527db8562e61bebc
vijaykumar211 <33776101+vijaykumar211@users.noreply.github.com>
2020-01-26 20:32:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 rm cs_domains_output.txt
 rm amass_out.txt
+rm massdns_output_1.txt
 ./amass enum --passive -d $1 -o amass_out.txt
 python commonspeak_list_generate.py $1
 sort amass_out.txt cs_domains_output.txt | uniq > amass_plus_cs_output.txt
+./massdns -r resolvers.txt -t A -o J -w massdns_output_1.txt amass_plus_cs_output.txt
