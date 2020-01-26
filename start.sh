c43a59db235c130398c4aebc7fab23a4f2e10f83
vijaykumar211 <33776101+vijaykumar211@users.noreply.github.com>
2020-01-26 19:56:09 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+rm cs_domains_output.txt
+#rm amass_out.txt
+#./amass enum --passive -d $1 -o amass_out.txt
+python commonspeak_list_generate.py $1
+sort amass_out.txt cs_domains_output.txt | uniq > amass_plus_cs_output.txt
