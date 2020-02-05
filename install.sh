2b761d7d3bf57122a3af44a04816d19e44865844
vijaykumar211 <33776101+vijaykumar211@users.noreply.github.com>
2020-02-05 13:21:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 sudo apt-get update -y;
 sudo apt-get install apache2 -y;
 sudo apt-get install build-essential -y;
-sudo apt install python -y;        
+sudo apt install python -y;   
+
+apt install python-pip -y;
 
 git clone https://github.com/blechschmidt/massdns.git;
 cd massdns;
