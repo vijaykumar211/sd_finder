2621c7005832b723de72cda284683e15d4a0063b
vijaykumar211 <33776101+vijaykumar211@users.noreply.github.com>
2020-02-05 13:19:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
-rm cs_domains_output.txt
-rm amass_out.txt
-rm massdns_output_1.txt
-rm amass_plus_cs_output.txt
-rm  massdns_output_2.txt
-rm wordlist_filtered.txt
-rm wordlist_filtered_1.txt
-./amass enum --passive -d $1 -o amass_out.txt
+rm *_output.txt
+./amass enum --passive -d $1 -o amass_output.txt
 python commonspeak_list_generate.py $1
-sort amass_out.txt cs_domains_output.txt | uniq > amass_plus_cs_output.txt
-#./massdns -r resolvers.txt -t A -o J -w massdns_output_1.txt amass_plus_cs_output.txt
-./massdns -r <(curl -s https://raw.githubusercontent.com/vijaykumar2111/freshdnsrecord/master/resolvers.txt) -t A -s 1000 -o S -w "massdns_output_2.txt" "amass_plus_cs_output.txt"
-awk -F ". " '{print $1}' "massdns_output_2.txt" > "wordlist_filtered.txt"
-awk '!seen[$0]++' wordlist_filtered.txt > wordlist_filtered_1.txt
+sort amass_output.txt cs_domains_output.txt | uniq > amass_plus_cs_output.txt
+cat amass_plus_cs_output.txt | shuf > amass_plus_cs_2_output.txt
+cat amass_plus_cs_output.txt | shuf > amass_plus_cs_3_output.txt
+./massdns -r <(curl -s https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt) -t A -s 1000 -o S -w "massdns_1_output.txt" "amass_plus_cs_output.txt"
+./massdns -r <(curl -s https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt) -t A -s 1000 -o S -w "massdns_2_output.txt" "amass_plus_cs_2_output.txt"
+./massdns -r <(curl -s https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt) -t A -s 1000 -o S -w "massdns_3_output.txt" "amass_plus_cs_3_output.txt"
+comm -12 <(sort massdns_1_output.txt) <(sort massdns_2_output.txt) > massdns_common_1_output.txt
+comm -12 <(sort massdns_common_1_output.txt) <(sort massdns_3_output.txt) > massdns_common_final_output.txt
+awk -F ". " '{print $1}' "massdns_common_final_output.txt" > "wordlist_filtered_output.txt"
+awk '!seen[$0]++' wordlist_filtered_output.txt > wordlist_filtered_final_output.txt
