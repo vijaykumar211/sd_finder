b555959ca4066bd902637511212358a0dd599a45
vijaykumar211 <33776101+vijaykumar211@users.noreply.github.com>
2020-02-05 13:48:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,3 +11,18 @@ comm -12 <(sort massdns_1_output.txt) <(sort massdns_2_output.txt) > massdns_com
 comm -12 <(sort massdns_common_1_output.txt) <(sort massdns_3_output.txt) > massdns_common_final_output.txt
 awk -F ". " '{print $1}' "massdns_common_final_output.txt" > "wordlist_filtered_output.txt"
 awk '!seen[$0]++' wordlist_filtered_output.txt > wordlist_filtered_final_output.txt
+
+altdns -i wordlist_filtered_final_output.txt -o altdns_output.txt -w altdns_wordlist.txt
+cat altdns_output.txt | shuf > altdns_2_output.txt
+cat altdns_output.txt | shuf > altdns_3_output.txt
+
+./massdns -r <(curl -s https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt) -t A -s 1000 -o S -w "altdns_massdns_1_output.txt" "altdns_output.txt"
+./massdns -r <(curl -s https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt) -t A -s 1000 -o S -w "altdns_massdns_2_output.txt" "altdns_2_output.txt"
+./massdns -r <(curl -s https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt) -t A -s 1000 -o S -w "altdns_massdns_3_output.txt" "altdns_3_output.txt"
+
+comm -12 <(sort altdns_massdns_1_output.txt) <(sort altdns_massdns_2_output.txt) > altdns_massdns_common_output.txt
+comm -12 <(sort altdns_massdns_common_output.txt) <(sort altdns_massdns_3_output.txt) > altdns_massdns_common_final_output.txt
+awk -F ". " '{print $1}' "altdns_massdns_common_final_output.txt" > "altdns_massdns_common_filtered_output.txt"
+awk '!seen[$0]++' altdns_massdns_common_filtered_output.txt > altdns_massdns_sorted_final_output.txt
+
+sort wordlist_filtered_final_output.txt altdns_massdns_sorted_final_output.txt | uniq > altdns_amass_final_output.txt
